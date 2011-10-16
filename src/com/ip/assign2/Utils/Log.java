331fbec27bb04f027dff6e66699b511fbb53a194
Daniel Grech <danielgrech91@gmail.com>
2011-10-17 10:34:29 +1100

    Added logging capabilities

---
+++ src/com/ip/assign2/Utils/Log.java
@@ -0,0 +1,11 @@
+package com.ip.assign2.Utils;
+
+public class Log {
+	public static void debug(String msg) {
+		System.err.println("DEBUG: " + msg);
+	}
+	
+	public static void info(String msg) {
+		System.err.println("INFO: " + msg);
+	}
+}
