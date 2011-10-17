561950c1a3b1176a71407222259cb73c624ddc29
Daniel Grech <danielgrech91@gmail.com>
2011-10-17 12:23:54 +1100

    Added logout handler

---
+++ src/com/ip/assign2/Servlets/LogoutServlet.java
@@ -0,0 +1,26 @@
+package com.ip.assign2.Servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet to handle user logout
+ */
+public class LogoutServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    public LogoutServlet() {
+        super();
+    }
+    
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Discard of
+		request.getSession().setAttribute("user", null);
+		
+		response.sendRedirect(request.getContextPath() + "/index.jsp");
+	}
+
+}
